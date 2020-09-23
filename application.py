037ab36a54311b1374b2a8705ca519e71b3f8e6f
torerik123 <62653782+torerik123@users.noreply.github.com>
2020-09-23 07:41:30 +0200

    Update application.py

--- application.py
+++ application.py
@@ -34,7 +34,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+# Configure CS50 Library to use Postgres database
 db = SQL("postgres://iefqlnbzsozhqq:5709d2f8dedf4ae4b7596761c7e35abe7522d6f4d412e890c7b4a9bf508eafbd@ec2-54-75-225-52.eu-west-1.compute.amazonaws.com:5432/d9mmsoq2fph46v")
 
 # Make sure API key is set
